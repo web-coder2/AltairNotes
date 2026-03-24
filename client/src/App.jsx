5e6d84b15e15297c9f5ed4088c040653a7418eb0
web-coder <yasher18@bk.ru>
2026-03-24 13:30:34 +0300

    сделал init фронтенда

---
+++ client/src/App.jsx
@@ -0,0 +1,24 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from './assets/vite.svg'
+import heroImg from './assets/hero.png'
+import './App.css'
+
+import axios from 'axios'
+import dayjs from 'dayjs'
+
+function App() {
+
+  return (
+
+    <>
+
+      <h3>Заметки Альтаира</h3>
+    
+    </>
+
+  )
+
+}
+
+export default App
